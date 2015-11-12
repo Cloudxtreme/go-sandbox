5f54327eb0de8df0c0ef46fe8f8fa7fdb1e49d6c
Koichi Shiraishi <zchee.io@gmail.com>
2015-11-12 15:47:43 +0900

    Add sorting int type
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ sorting/sorting_int.go
@@ -0,0 +1,53 @@
+// Original code is https://gobyexample.com/sorting-by-functions
+
+// Sometimes we'll want to sort a collection by something
+// other than its natural order. For example, suppose we
+// wanted to sort strings by their length instead of
+// alphabetically. Here's an example of custom sorts
+// in Go.
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// In order to sort by a custom function in Go, we need a
+// corresponding type. Here we've created a `ByLength`
+// type that is just an alias for the builtin `[]string`
+// type.
+// type IntSlice []int
+
+// We implement `sort.Interface` - `Len`, `Less`, and
+// `Swap` - on our type so we can use the `sort` package's
+// generic `Sort` function. `Len` and `Swap`
+// will usually be similar across types and `Less` will
+// hold the actual custom sorting logic. In our case we
+// want to sort in order of increasing string length, so
+// we use `len(s[i])` and `len(s[j])` here.
+// func (s IntSlice) Len() int {
+// 	return len(s)
+// }
+// func (s IntSlice) Swap(i, j int) {
+// 	s[i], s[j] = s[j], s[i]
+// }
+// func (s IntSlice) Less(i, j int) bool {
+// 	return len(s[i]) < len(s[j])
+// }
+
+// With all of this in place, we can now implement our
+// custom sort by casting the original `fruits` slice to
+// `ByLength`, and then use `sort.Sort` on that typed
+// slice.
+func main() {
+	// i := []int{5, 2, 6, 3, 1, 4}
+	// // sort.IntSlice に型変換している
+	// // sort.Interface が満たされる
+	// sort.Sort(sort.IntSlice(i))
+	// fmt.Println(i) // [1 2 3 4 5 6]numbers := []int{14, 3, 5}
+
+	numbers := []int{14, 3, 5}
+	sort.Sort(sort.IntSlice(numbers))
+	fmt.Println(numbers)
+}
